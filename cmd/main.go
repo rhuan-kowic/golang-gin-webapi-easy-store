a5abd58b7b758091e1499abf100f10d587d912f1
Rhuan Kowic Santos <dev.rhuankowic@gmail.com>
2024-12-17 09:11:12 -0300

    Adicionando product repository e endpoint products.

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/rhuan-kowic/golang-gin-webapi-easy-store/db"
+	"github.com/rhuan-kowic/golang-gin-webapi-easy-store/repository"
 )
 
 func main() {
@@ -16,13 +17,22 @@ func main() {
 		log.Fatal("Error connecting to the database:", err)
 	}
 	defer db.Close()
-
 	fmt.Println("Conexão com o banco de dados estabelecida com sucesso!")
 
+	ProductRepository := repository.NewProductRepository(db)
 	server.GET("/home", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, gin.H{
 			"message": "Easy Store",
 		})
+	})
+
+	server.GET("/products", func(ctx *gin.Context) {
+		products, err := repository.GetProducts(&ProductRepository)
+		if err != nil {
+			ctx.JSON(http.StatusInternalServerError, err)
+			return
+		}
+		ctx.JSON(http.StatusOK, products)
 	})
 	server.Run(":3000")
 }
---
+++ repository/product_repository.go
@@ -0,0 +1,49 @@
+package repository
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/rhuan-kowic/golang-gin-webapi-easy-store/model"
+)
+
+type ProductRepository struct {
+	connection *sql.DB
+}
+
+func NewProductRepository(connection *sql.DB) ProductRepository {
+	return ProductRepository{
+		connection: connection,
+	}
+}
+
+func GetProducts(pr *ProductRepository) ([]model.Product, error) {
+	query := "SELECT id_product, name, description, price, stock_quantity, minimum_stock FROM products"
+
+	rows, err := pr.connection.Query(query)
+	if err != nil {
+		fmt.Println(err)
+		return []model.Product{}, err
+	}
+
+	var productList []model.Product
+	var productObj model.Product
+
+	for rows.Next() {
+		err = rows.Scan(
+			&productObj.ID,
+			&productObj.Name,
+			&productObj.Description,
+			&productObj.Price,
+			&productObj.StockQuantity,
+			&productObj.MinimumStock)
+
+		if err != nil {
+			fmt.Println(err)
+			return []model.Product{}, err
+		}
+		productList = append(productList, productObj)
+	}
+	rows.Close()
+	return productList, nil
+}
